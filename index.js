74a1525323fc1961600d634d67633a0d55e5aff0
mohammad khakshoor <mkhbeats@gmail.com>
2023-06-07 23:30:12 +0330

    CREAT: add basic form validation to E-mail addess input

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const fromButton = document.querySelector(".from__btn");
 const dissmissMessageButton = document.querySelector(".popup__dismiss");
 const popup = document.querySelector(".popup");
-const fromInput = document.querySelector(".from__input");
-const fromContainer = document.querySelector(".from");
+const formInput = document.querySelector(".from__input");
+const formContainer = document.querySelector(".from");
 
-fromContainer.addEventListener("submit", function (e) {
+fromButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const email = fromInput.value;
-    console.log(email);
+    const email = formInput.value;
 
     if (email.includes("@")) {
         popup.classList.remove("hidden");
         popup.classList.add("animation");
+    }
+    if (!email || !email.includes("@")) {
+        alert("Please enter a valid E-mail address");
     }
 });
 
