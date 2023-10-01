07f5e93f5746a7bd76c212c2c737c69c1ad10e59
mohammad khakshoor <mkhbeats@gmail.com>
2023-10-01 20:39:58 +0330

    UPDATE: add RegEx for email input valdiation

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const dissmissMessageButton = document.querySelector(".popup__dismiss");
 const popup = document.querySelector(".popup");
 const formInput = document.querySelector(".from__input");
 const formContainer = document.querySelector(".from");
+console.log(formInput.value);
+// regEx for email validation
+const regEx =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 fromButton.addEventListener("click", function (e) {
-    e.preventDefault();
+  e.preventDefault();
+  const email = formInput.value;
+  const invalidPopup = document.getElementsByClassName("from__invalid-popup");
 
-    const email = formInput.value;
-    const invalidPopup = document.getElementsByClassName("from__invalid-popup");
-    if (!email || !email.includes("@")) {
-        // if the invalid popup does not exist in the dom , add it once and no more
-        if (!invalidPopup.length) {
-            const html = `
-        <label class="from__invalid-popup animate" for="email">Invalid E-mail</label>
-        `;
-            formContainer.insertAdjacentHTML("afterbegin", html);
-        }
-    }
-    if (email && email.includes("@")) {
-        popup.classList.remove("hidden");
-        popup.classList.add("animation");
-        // delete the invalid E-mail popup when the address is correct
-        invalidPopup[0].remove();
-    }
+  invalidPopup.length > 0 && invalidPopup?.[0].remove();
+
+  if (regEx.test(email)) {
+    popup.classList.remove("hidden");
+    popup.classList.add("animation");
+  } else {
+    const html = `
+    <label class="from__invalid-popup animate" for="email">Invalid E-mail</label>
+    `;
+    formContainer.insertAdjacentHTML("afterbegin", html);
+  }
 });
 
 dissmissMessageButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    popup.classList.add("hidden");
+  e.preventDefault();
+  popup.classList.add("hidden");
 });
